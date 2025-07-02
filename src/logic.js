bb0faea93dd90064c217c4624b9b746dcfefbbeb
Nerter <terence.chardes@gmail.com>
2025-07-02 18:35:59 +0200

    Modification des l'UI du site et optimisations générale notamment pour rendre le code plus lisible et ordonné

--- src/logic.js
+++ src/logic.js
@@ -1,9 +1,13 @@
-﻿fetch('/misc/banner.html')
+﻿//----------BANNER-----------
+
+fetch('/misc/banner.html')
 .then(response => response.text())  
 .then(data => {
   document.getElementById('banner-container').innerHTML = data;
 });
 
+
+//----------DESCRIPTION-----------
 
 setDescriptionContent('/assets/images/ss/description.txt', 'ss');
 setDescriptionContent('/assets/images/sad/description.txt', 'sad');
@@ -12,9 +16,7 @@ setDescriptionContent('/assets/images/mp/description.txt', 'mp');
 setDescriptionContent('/assets/images/mg/description.txt', 'mg');
 setDescriptionContent('/assets/images/mv/description.txt', 'mv');
 
-
-
-function setDescriptionContent(descriptionPath, id){
+function setDescriptionContent(descriptionPath, id){ //set project description from a description file
   fetch(descriptionPath)
   .then(response => {
     if (!response.ok) {
@@ -27,44 +29,36 @@ function setDescriptionContent(descriptionPath, id){
     document.getElementById(id).textContent = data;
   })
 }
+
+
+//----------LAST UPDATE DATE-----------
+
+let fileName = window.location.pathname.split('/').pop();
+
+if(fileName == "ss.html"){setLastUpdateDate('61285052');}
+else if(fileName == "mp.html"){setLastUpdateDate('61286686');}
+else if(fileName == "sad.html"){setLastUpdateDate('61286493');}
+else if(fileName == "mv.html"){setLastUpdateDate('62698256');}
+else if(fileName == "mg.html"){setLastUpdateDate('66064852');}
+else if(fileName == "ps.html"){setLastUpdateDate('69538029');}
+
+
      
-async function getLastUpdateDate(projectId) {
+async function setLastUpdateDate(projectId) { // set the date of the last gitlab commit from a project 
   const response = await fetch(`https://gitlab.com/api/v4/projects/${projectId}/repository/commits`)
 
-  const data = await response.json();
-  const date = extractDate(data);
+  const commit = await response.json();
+  const date = getCommitDate(commit);
 
   const outputDiv = document.getElementById('lastUpdateDate');
   outputDiv.textContent = `Dernière Mise à Jour : ${date}`;
 }
 
-function extractDate(data) {
-
-  const entry = data[0]; 
-  message =  entry.message
+function getCommitDate(data) {//extract the date from the last commit of commits.json
+  const entry = data[0]; //last commit
   let dateArray = entry.committed_date.substring(0, 10).split('-');
-  return dateArray[2] + '/' + dateArray[1] + '/' + dateArray[0];
+  return dateArray[2] + '/' + dateArray[1] + '/' + dateArray[0]; // jj/mm/aaaa french format lets go
   
-}
-
-let fileName = window.location.pathname.split('/').pop();
-if(fileName == "ss.html"){
-  getLastUpdateDate('61285052');
-}
-else if(fileName == "mp.html"){
-  getLastUpdateDate('61286686');
-}
-else if(fileName == "sad.html"){
-  getLastUpdateDate('61286493');
-}
-else if(fileName == "mv.html"){
-  getLastUpdateDate('62698256');
-}
-else if(fileName == "mg.html"){
-  getLastUpdateDate('66064852');
-}
-else if(fileName == "ps.html"){
-  getLastUpdateDate('69538029');
 }
 
 
