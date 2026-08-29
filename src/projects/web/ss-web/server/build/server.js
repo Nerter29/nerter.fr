c100df61cfdd519002cac775c8274d5dfb3abea3
Nerter <terence.chardes@gmail.com>
2026-08-29 18:13:16 +0200

    updated sh

--- src/projects/web/ss-web/server/build/server.js
+++ src/projects/web/ss-web/server/build/server.js
@@ -1,2 +1,27 @@
 "use strict";
-console.log("a");
+const express = require("express");
+const { exec } = require("child_process");
+const app = express();
+const port = 3000;
+function isScorePayload(obj) {
+    return (typeof obj.token !== 'string' || typeof obj.score !== 'number');
+}
+//so any browser can access the json
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+});
+app.post("/api", async (req, res) => {
+    if (isScorePayload(req.body)) {
+        const payload = req.body;
+        console.log(payload);
+        res.status(200).json(payload);
+    }
+    else {
+        console.log("fini");
+        res.status(500);
+    }
+});
+app.listen(port, () => {
+    console.log("Dashboard running");
+});
